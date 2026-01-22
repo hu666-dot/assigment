b4262e6c25a0b2ff569430b645e23be4a94c7cb7
Hung <hu6287834@gmail.com>
2026-01-22 09:58:26 +0700

    Initial ASP.NET Core MVC calculator project

---
+++ Assigment2_Calculate/Controllers/CalculatorController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Assignment1.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        // GET: /Calculator
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // Core function – mỗi branch chỉ sửa hàm này
+        public int Calculate(int a, int b)
+        {
+            return 0; // TODO: implement theo từng branch
+        }
+
+        // POST: /Calculator/Result
+        [HttpPost]
+        public IActionResult Result(int a, int b)
+        {
+            int result = Calculate(a, b);
+            ViewBag.Result = result;
+            return View("Index");
+        }
+    }
+}
---
+++ Assigment2_Calculate/Models/SIGN.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Assigment2_Calculate.Models
+{
+    public class SIGN : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
